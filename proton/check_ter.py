e506c3862a9cb0fc0616ccbdd0e8f3590eb2ef2d
Kumar Madhukar <madhukar.k.singh@gmail.com>
2025-08-29 11:54:01 +0530

    Update check_ter.py

--- proton/check_ter.py
+++ proton/check_ter.py
@@ -1,12 +1,13 @@
 """
-Given a program, check and return term, non-term or unk
-"""
-
-"""
 PROTON
 (C) 2022-2042, Kumar Madhukar
 Department of Computer Science and Engineering, Indian Institute of Technology Delhi
 """
+
+"""
+Given a program, check and return term, non-term or unk
+"""
+
 
 import os
 import os.path
