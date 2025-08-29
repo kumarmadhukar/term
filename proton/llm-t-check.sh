6a1ad2aba9dec30e42d7e65499f011a6dab113db
Kumar Madhukar <madhukar.k.singh@gmail.com>
2025-08-29 11:55:14 +0530

    Update llm-t-check.sh

--- proton/llm-t-check.sh
+++ proton/llm-t-check.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # tool
 
+#
+# PROTON
+# (C) 2022-2042, Kumar Madhukar
+# Department of Computer Science and Engineering, Indian Institute of Technology Delhi
+#
+
+
 # A script wrapping around the term check python script to interface better with
 # rest of proton
 
